49cf201ee9e12148ac25bf2af8a43bd2d4b3af16
erkutbas <ebas1@binghamton.edu>
2021-09-14 21:35:52 +0300

    Missing CharacterId Property Appended

--- Sources/MarvelDomainLayer/CharacterListOperations/Models/ComicsDataRequest.swift
+++ Sources/MarvelDomainLayer/CharacterListOperations/Models/ComicsDataRequest.swift
@@ -9,15 +9,18 @@ import Foundation
 
 public struct ComicsDataRequest: Codable {
     
+    public var characterId: Int
     public var orderBy: String = "-onsaleDate"
     public var limit: Int = 10
     
-    public init(orderBy: String = "-onsaleDate", limit: Int = 10) {
+    public init(characterId: Int, orderBy: String = "-onsaleDate", limit: Int = 10) {
+        self.characterId = characterId
         self.orderBy = orderBy
         self.limit = limit
     }
     
     enum CodingKeys: String, CodingKey {
+        case characterId
         case orderBy
         case limit
     }
