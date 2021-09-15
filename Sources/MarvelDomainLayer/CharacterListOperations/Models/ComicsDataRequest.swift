be797c3b8e80cf8e179c9a19f87b970011eaaae8
erkutbas <ebas1@binghamton.edu>
2021-09-15 19:20:09 +0300

    ComicsRequest Updated

--- Sources/MarvelDomainLayer/CharacterListOperations/Models/ComicsDataRequest.swift
+++ Sources/MarvelDomainLayer/CharacterListOperations/Models/ComicsDataRequest.swift
@@ -10,18 +10,24 @@ import Foundation
 public struct ComicsDataRequest: Codable {
     
     public var characterId: Int
+    public var dateRange: String
     public var orderBy: String = "-onsaleDate"
     public var limit: Int = 10
+    public var format: String = "comic"
     
-    public init(characterId: Int, orderBy: String = "-onsaleDate", limit: Int = 10) {
+    public init(characterId: Int, dateRange: String, orderBy: String = "-onsaleDate", limit: Int = 10, format: String = "comic") {
         self.characterId = characterId
+        self.dateRange = dateRange
         self.orderBy = orderBy
         self.limit = limit
+        self.format = format
     }
     
     enum CodingKeys: String, CodingKey {
         case characterId
         case orderBy
         case limit
+        case dateRange
+        case format
     }
 }
